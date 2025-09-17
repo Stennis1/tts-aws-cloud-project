cab564e7bc096fa44097b08b6b9aa39ae6694322
Stennis1 <stennismith@gmail.com>
2025-09-17 00:25:57 +0000

    chore: build underlying project structure with terraform files

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+module "s3_web" {
+  source      = "./modules/s3_web"
+  bucket_name = "tts-project-azubi-stennis-009"
+}
+
+module "cloudfront" {
+  source              = "./modules/cloudfront"
+  bucket_domain_name  = module.s3_web.static_site_endpoint
+  bucket_id           = module.s3_web.static_site_bucket_name
+  bucket_arn          = module.s3_web.static_site_bucket_arn
+  price_class         = "PriceClass_100"
+  default_root_object = "index.html"
+}
